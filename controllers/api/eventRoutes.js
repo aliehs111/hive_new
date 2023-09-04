b472bae72517fd262dc9ed51657551bfd04ca179
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-09-04 09:56:47 -0400

    fixes edit and search

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -20,128 +20,47 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 //get the event that the user wants to edit
-router.get('/edit/:id', withAuth, async (req, res) => {
+
+router.put('/:id', withAuth, async (req, res) => {
   try {
     const eventId = req.params.id;
-    // Fetch the event data by ID
-    const eventData = await Event.findOne({
-      where: { id: eventId },
-     
-    });
-
-    if (!eventData) {
-      // Handle event not found
-      res.render('error', {
-        message: 'Event not found',
-      });
-      return;
-    }
-
-    const event = eventData.get({ plain: true });
-    res.render('editevent', {
-      loggedIn: req.session.logged_in,
-      event,
-    });
-  } catch (error) {
-    // Handle error
-    res.render('error', {
-      message: 'An error occurred while fetching event data',
-    });
-  }
-});
-
-router.put('/edit/:id', withAuth, async (req, res) => {
-  try {
-    const eventId = req.params.id;
-    const { name, description, date_scheduled, /* other attributes */ } = req.body;
 
     // Update the event data in the database
-    const updatedEvent = await Event.update(
-      {
-        name,
-        description,
-        date_scheduled,
-        // Update other attributes as needed
-      },
-      {
-        where: { id: eventId },
-      }
-    );
+    const updatedEvent = await Event.update(req.body, {
+      where: { id: eventId },
+    });
 
-    if (updatedEvent[0] === 0) {
-      // Handle event not found or failed update
-      res.render('error', {
-        message: 'Event not found or update failed',
-      });
-      return;
-    }
-
-    // Event was successfully updated
-    res.redirect('/'); // Redirect to the homepage or another relevant page
+    res.json({
+      message: 'Event updated successfully',
+    });
   } catch (error) {
     // Handle error
-    res.render('error', {
-      message: 'An error occurred while updating the event',
+    res.status(500).json({
+      message: 'No event found with this id',
     });
   }
 });
-//////////////////
-// // update an event
-// router.put('/:id', withAuth, async (req, res) => {
-//   try {
-//     const updatedEvent = await Event.update(req.body, {
-//       where: {
-//       id: req.params.id,
-//       user_id: req.session.user.id, //make sure event belongs to user - maybe not necessary?
-//     },
-//   });
 
-//   if (!updatedEvent[0]) {
-//     res.status(404).json({ message: 'No event found with this id' });
-//     return;
-//   }
-
-//   res.status(200).json(updatedEvent);
-// } catch (err) {
-//   res.status(400).json(err);
-// }
-// });
-//////////////////
-// search for events
-
-router.get('/events/', withAuth, async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
-      const query = req.query.query; // Retrieve search query from request
-      const eventsData = await Event.findAll({
-        where: {
-          $or: [
-            { name: { $like: `%${query}%` } },
-            { description: { $like: `%${query}%` } },
-            // Add more attributes as needed
-          ],
-        },
-        attributes: ['id', 'name', 'description', 'date_scheduled', 'image'],
-        include: [
-          {
-            model: User,
-            attributes: ['user_name'],
-          },
-        ],
-      });
-      const events = eventsData.map((event) => event.get({ plain: true }));
-      res.render('searchresults', {
-          loggedIn: req.session.logged_in,
-          events,
-      });
+    const eventId = req.params.id;
+    //delete event
+    const deletedEvent = await Event.destroy({
+      where: { id: eventId },
+    });
+    //response
+    res.json({
+      message: 'Event deleted successfully',
+    });
   } catch (error) {
-      res.render('searchresults', {
-          loggedIn: req.session.logged_in,
-          error: 'Failed to load search results'
-      });
+    // Handle error
+    res.status(500).json({
+      message: 'Error deleting event',
+    });
   }
 });
 
-
+    
 
 
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Event } = require('../models');
+const { Op } = require('sequelize');
 
 const withAuth = require('../utils/auth');
 
@@ -26,15 +27,45 @@ router.get('/', withAuth, async function (req, res) {
     });
   }
 });
-
-
+router.get('/events/search', withAuth, async (req, res) => {
+  try {
+    const query = req.query.search; // Retrieve search query from request
+    const eventsData = await Event.findAll({
+      where: {
+        [Op.or]: [
+          { name: { [Op.like]: `%${query}%` } },
+          { description: { [Op.like]: `%${query}%` } },
+          // Add more attributes as needed
+        ],
+      },
+      attributes: ['id', 'name', 'description', 'date_scheduled', 'image'],
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+      ],
+    });
+    const events = eventsData.map((event) => event.get({ plain: true }));
+    res.render('searchresults', {
+      loggedIn: req.session.logged_in,
+      events,
+    });
+  } catch (error) {
+    res.render('searchresults', {
+      loggedIn: req.session.logged_in,
+      error: 'Failed to load search results',
+    });
+  }
+});
+//event details
 router.get('/events/:id', withAuth, async function (req, res) {
   try {
     const eventData = await Event.findOne({
       where: {
         id: req.params.id,
       },
-      attributes: ['id', 'name', 'description', 'date_scheduled', 'image'],
+      attributes: ['id', 'name', 'description', 'date_scheduled', 'image', 'user_id'],
       include: [
         {
           model: User,
@@ -44,9 +75,11 @@ router.get('/events/:id', withAuth, async function (req, res) {
     });
     const event = eventData.get({ plain: true });
     console.log(event);
+    const isOwner = event.user_id === req.session.user_id;
     res.render('eventdetails', {
       loggedIn: req.session.logged_in,
       event,
+      isOwner,
     });
   } catch (error) {
     res.render('eventdetails', {
@@ -56,6 +89,41 @@ router.get('/events/:id', withAuth, async function (req, res) {
   }
 });
 
+router.get('/events/edit/:id', withAuth, async (req, res) => {
+  try {
+    const eventId = req.params.id;
+    // Fetch the event data by ID
+    const eventData = await Event.findOne({
+      where: { id: eventId },
+     
+    });
+    const isOwner = eventData.user_id === req.session.user_id;
+    if (!isOwner) {
+      res.redirect('/events/' + eventId);
+      return;
+    }
+    if (!eventData) {
+      // Handle event not found
+      res.render('error', {
+        message: 'Event not found',
+      });
+      return;
+    }
+
+    const event = eventData.get({ plain: true });
+
+    event.date_scheduled = eventData.date_scheduled.toISOString().split('T')[0];
+    res.render('editevent', {
+      loggedIn: req.session.logged_in,
+      event,
+    });
+  } catch (error) {
+    // Handle error
+    res.render('error', {
+      message: 'An error occurred while fetching event data',
+    });
+  }
+});
 
 
 router.get('/signup', function (req, res) {
